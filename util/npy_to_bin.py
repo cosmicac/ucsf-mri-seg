bd8151bb88e0492d8cb9e003da78a90db0a7a14b
Ubuntu <ubuntu@ip-172-31-29-255.us-west-2.compute.internal>
2017-01-14 11:36:34 +0000

    made 2ch bins

--- util/npy_to_bin.py
+++ util/npy_to_bin.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def flatten_and_bin(imgs, labels, save_path):
-	n, h, w, d = imgs.shape 
+	n, h, w, d, nc = imgs.shape 
 
 	# flatten images array
-	imgs = imgs.reshape((n, h*w*d))
+	imgs = imgs.reshape((n, h*w*d*nc))
 	labels = labels.reshape((labels.shape[0], 1))
 
 	# combine stack them horizontally
@@ -17,10 +17,9 @@ def flatten_and_bin(imgs, labels, save_path):
 
 if __name__ == '__main__':
 
-	"""
 	# load images and cast as uint16
-	imgs = np.load('../../data/datasets/train_mix.npy').astype('uint16')
-	labels = np.load('../../data/datasets/train_labels_mix.npy').astype('uint16')
+	imgs = np.load('../../data/datasets/train_2ch.npy').astype('uint16')
+	labels = np.load('../../data/datasets/train_labels_2ch.npy').astype('uint16')
 
 	
 	# todo don't hardcode this
@@ -37,16 +36,15 @@ if __name__ == '__main__':
 	labels4 = labels[60000:80000]
 	labels5 = labels[80000:100000]	
 
-	flatten_and_bin(imgs1, labels1, '../../data/datasets/bins/train_and_label_mix_batch_1.bin')
-	flatten_and_bin(imgs2, labels2, '../../data/datasets/bins/train_and_label_mix_batch_2.bin')
-	flatten_and_bin(imgs3, labels3, '../../data/datasets/bins/train_and_label_mix_batch_3.bin')
-	flatten_and_bin(imgs4, labels4, '../../data/datasets/bins/train_and_label_mix_batch_4.bin')
-	flatten_and_bin(imgs5, labels5, '../../data/datasets/bins/train_and_label_mix_batch_5.bin')
-	"""
+	flatten_and_bin(imgs1, labels1, '../../data/datasets/bins/train_and_label_2ch_batch_1.bin')
+	flatten_and_bin(imgs2, labels2, '../../data/datasets/bins/train_and_label_2ch_batch_2.bin')
+	flatten_and_bin(imgs3, labels3, '../../data/datasets/bins/train_and_label_2ch_batch_3.bin')
+	flatten_and_bin(imgs4, labels4, '../../data/datasets/bins/train_and_label_2ch_batch_4.bin')
+	flatten_and_bin(imgs5, labels5, '../../data/datasets/bins/train_and_label_2ch_batch_5.bin')
 
+	"""
 	img8d9 = np.load('../../data/datasets/pred_arrs/img8d9.npy').astype('uint16')
 	img8d9_labels = np.load('../../data/datasets/pred_arrs/img8d9_labels.npy').astype('uint16')
 	flatten_and_bin(img8d9, img8d9_labels, '../../data/datasets/bins/img8d9_and_label_batch_1.bin')
-
-
+	"""
 
