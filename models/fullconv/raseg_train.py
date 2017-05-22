ad85b5b60db7cf9bc59a557c0665b5610b84561c
Tom Li <cosmicac@berkeley.edu>
2017-05-22 22:38:03 +0000

    need more params for bn

--- models/fullconv/raseg_train.py
+++ models/fullconv/raseg_train.py
@@ -5,7 +5,7 @@ import raseg_model
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '../../../models/raseg_train_fullimg_bme_2',
+tf.app.flags.DEFINE_string('train_dir', '../../../models/raseg_train_fullimg_bmebn',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 1000000,
