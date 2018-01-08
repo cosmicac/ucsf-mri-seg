74f4da6d0c8a2d3dad1f1795fe589832516ff377
Tom Li <cosmicac@berkeley.edu>
2018-01-08 00:47:23 +0000

    deleted unused stuff

--- eval_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {0..19}
-do
-   python3 raseg_eval.py --imgn=30 --depthn=$i
-done
--- models/fullconv/val_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#for i in $(seq 0 7);
-validx=(0 1 2 3 54 55 56 57 58 59 60)
-for i in ${validx[@]}; 
-do
-  python raseg_predict.py --imgn=$i
-done
-cd postprocess
-python postprocess.py
